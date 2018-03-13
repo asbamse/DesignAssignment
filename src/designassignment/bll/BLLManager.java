9ccb7a929259a6ad8ca6ba83a3508eebae7400cf
Applemelon <32298607+Applemelon@users.noreply.github.com>
2018-03-13 11:31:12 +0100

    Login encryption and connection through BLL

--- src/designassignment/bll/BLLManager.java
+++ src/designassignment/bll/BLLManager.java
@@ -10,33 +10,33 @@ import designassignment.dal.DALException;
 import designassignment.dal.DALFacade;
 import designassignment.dal.DALManager;
 import designassignment.dal.DALManagerXML;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Asbamz
  */
-public class BLLManager implements BLLFacade
-{
+public class BLLManager implements BLLFacade {
+
     private static BLLManager INSTANCE = null;
     private DALFacade dal;
 
     /**
      * BLLManager constructor.
      */
-    private BLLManager()
-    {
+    private BLLManager() {
         this.dal = DALManager.getInstance();
     }
 
-    public static BLLManager getInstance()
-    {
-        if (INSTANCE == null)
-        {
-            synchronized (BLLManager.class)
-            {
-                if (INSTANCE == null)
-                {
+    public static BLLManager getInstance() {
+        if (INSTANCE == null) {
+            synchronized (BLLManager.class) {
+                if (INSTANCE == null) {
                     INSTANCE = new BLLManager();
                 }
             }
@@ -46,44 +46,68 @@ public class BLLManager implements BLLFacade
 
     /**
      * Send message.
+     *
      * @param message
      * @return
      */
     @Override
-    public Message sendMessage(String message) throws BLLException
-    {
-        try
-        {
+    public Message sendMessage(String message) throws BLLException {
+        try {
             return dal.sendMessage(message);
-        }
-        catch (DALException ex)
-        {
+        } catch (DALException ex) {
             throw new BLLException(ex.getMessage(), ex.getCause());
         }
     }
 
     @Override
-    public List<Message> getMessages() throws BLLException
-    {
-        try
-        {
+    public List<Message> getMessages() throws BLLException {
+        try {
             return dal.getMessages();
-        }
-        catch (DALException ex)
-        {
+        } catch (DALException ex) {
             throw new BLLException(ex.getMessage(), ex.getCause());
         }
     }
 
     @Override
     public void deleteMessage(Message thisMessage) throws BLLException {
-         try
-        {
+        try {
             dal.deleteMessage(thisMessage);
-        }
-        catch (DALException ex)
-        {
+        } catch (DALException ex) {
             throw new BLLException(ex.getMessage(), ex.getCause());
+        }
+    }
+
+    @Override
+    public void createUser(String username, String email, String password) throws BLLException {
+        try {
+            String base = password;
+            
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            
+            byte[] hash = digest.digest(base.getBytes("UTF-8"));
+            
+            StringBuffer hexString = new StringBuffer();
+            
+            for (int i = 0; i < hash.length; i++) {
+                
+                String hex = Integer.toHexString(0xff & hash[i]);
+                
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+                
+                hexString.append(hex);
+            }
+            
+            String encryptedPassword = hexString.toString();
+            
+            dal.addUser(email, email, encryptedPassword);
+        } 
+        catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(BLLManager.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+        catch (UnsupportedEncodingException ex) {
+            Logger.getLogger(BLLManager.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
