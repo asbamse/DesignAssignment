38e8a1c4a52daec8810bc4c0d0a9544fe1ae25ca
Asbjørn Mansa Jensen <asbj0378@easv365.dk>
2018-03-19 12:18:17 +0100

    InputValidationFactory

---
+++ src/designassignment/gui/model/inputvalidation/MergeValidations.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package designassignment.gui.model.inputvalidation;
+
+/**
+ *
+ * @author Asbamz
+ */
+public class MergeValidations extends AbstractInputValidation
+{
+    private final InputValidation FIRST;
+    private final InputValidation SECOND;
+
+    public MergeValidations(InputValidation first, InputValidation second)
+    {
+        this.FIRST = first;
+        this.SECOND = second;
+    }
+
+    @Override
+    public boolean validateInput(String input)
+    {
+        boolean first = FIRST.validateInput(input);
+        boolean second = SECOND.validateInput(input);
+        if (first && second)
+        {
+            return true;
+        }
+        else if (first)
+        {
+            validationMessage = SECOND.getValidationMessage();
+            return false;
+        }
+        else if (second)
+        {
+            validationMessage = FIRST.getValidationMessage();
+            return false;
+        }
+        else
+        {
+            validationMessage = FIRST.getValidationMessage() + " and " + SECOND.getValidationMessage();
+            return false;
+        }
+    }
+}
