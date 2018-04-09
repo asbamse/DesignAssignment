d4552d2694ebfec3a80b9dd9959ca27bf8122254
JanvanZetten <33052378+JanvanZetten@users.noreply.github.com>
2018-04-09 13:26:06 +0200

    now also previuos in iterator

--- src/designassignment/bll/Collaborator/EndlessCollaboratorIterater.java
+++ src/designassignment/bll/Collaborator/EndlessCollaboratorIterater.java
@@ -8,7 +8,6 @@ package designassignment.bll.Collaborator;
 import designassignment.be.User;
 import designassignment.bll.BLLException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -39,6 +38,26 @@ public class EndlessCollaboratorIterater implements IteratorWithMultiNext<User>{
             currentIndex++;
             if (currentIndex >= collaborators.size()){
                 currentIndex = 0;
+            }
+        }
+        return nextColabs;
+    }
+
+    @Override
+    public List<User> previous(int count) throws BLLException {
+        if (collaborators == null || collaborators.size()<1){
+            throw new BLLException("There are no collaborators in the list");
+        }
+        int startIndex = currentIndex;
+        boolean first = true;
+        List<User> nextColabs = new ArrayList<>();
+        while(currentIndex <= startIndex - count && (first||currentIndex == startIndex)){
+            nextColabs.add(collaborators.get(currentIndex));
+            
+            first = false;
+            currentIndex--;
+            if (currentIndex < 0){
+                currentIndex = collaborators.size()-1;
             }
         }
         return nextColabs;
