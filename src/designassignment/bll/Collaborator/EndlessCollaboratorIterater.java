47b7c161b3e2fca8ded6d5e2c71ca1bfbdeab43a
JanvanZetten <33052378+JanvanZetten@users.noreply.github.com>
2018-04-09 12:38:19 +0200

    iterator basic implementation

--- src/designassignment/bll/Collaborator/EndlessCollaboratorIterater.java
+++ src/designassignment/bll/Collaborator/EndlessCollaboratorIterater.java
@@ -24,7 +24,7 @@ public class EndlessCollaboratorIterater implements Iterator<User>{
     
     @Override
     public boolean hasNext() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return true;
     }
 
     @Override
