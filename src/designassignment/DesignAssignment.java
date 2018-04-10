c8a69eaa17a6c166b477e91e640fa38df0bf5c5d
Asbjørn Mansa Jensen <asbj0378@easv365.dk>
2018-04-10 11:23:14 +0200

    Ups, wrong file

--- src/designassignment/DesignAssignment.java
+++ src/designassignment/DesignAssignment.java
@@ -26,7 +26,7 @@ public class DesignAssignment extends Application
     @Override
     public void start(Stage stage) throws Exception
     {
-        Parent root = FXMLLoader.load(getClass().getResource("gui/view/UserView.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("gui/view/LoginView.fxml"));
 
         Scene scene = new Scene(root);
 
